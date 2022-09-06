95695d20a6ff8a97f07ef4df438862c382ae3683
Thiago Odilon <thiago-almeida05@hotmail.com>
2022-09-06 08:43:03 -0300

    :blush: Correção no modal

--- front-end/src/views/Cadastrados.vue
+++ front-end/src/views/Cadastrados.vue
@@ -11,52 +11,54 @@
         :fields="fields"
         :current-page="currentPage"
         @row-clicked = "editar">
-        <template #cell(buttons)>
-          <div class="div-todos-ico">
-              <div class="div-icones">
-                <b class="margem-entre">
-                  <b-icon-check class="icons" scale="2" />
-                  <b class="finalizar">finalizar</b>
-                </b>
-              </div>
-              <div class="div-icones">
-                <a @click.prevent="excluir" >
+          <template #cell(buttons)>
+            <div class="div-todos-ico">
+                <div class="div-icones">
                   <b class="margem-entre">
-                    <b-icon-camera class="icons" scale="1.2" />
-                    <b class="fotos">fotos</b>
+                    <b-icon-check class="icons" scale="2" />
+                    <b class="finalizar">finalizar</b>
                   </b>
-                </a>
+                </div>
+                <div class="div-icones">
+                  <a @click.prevent="excluir" >
+                    <b class="margem-entre">
+                      <b-icon-camera class="icons" scale="1.2" />
+                      <b class="fotos">fotos</b>
+                    </b>
+                  </a>
+                </div>
+                <div class="div-icones">
+                  <a @click.prevent="deletar" >
+                    <b-icon-trash class="icons" scale="1"/>
+                    <b class="excluir">excluir</b>
+                  </a>
+                </div>     
               </div>
-              <div class="div-icones">
-                <a @click.prevent="deletar" >
-                  <b-icon-trash class="icons" scale="1"/>
-                  <b class="excluir">excluir</b>
-                </a>
-              </div>     
-            </div>
-        </template>
-      </b-table>
+          </template>
+        </b-table>
+        <b-modal id = "modal-editar" hide-footer>
+          <template #modal-title>
+            Item cadastrado
+          </template>
+          <b-form-input v-model = "dados.status"></b-form-input>
+          <b-form-input v-model = "dados.categoria" class="mt-2"></b-form-input>
+          <b-form-input v-model = "dados.descricao" class="mt-2"></b-form-input>
 
-      <b-modal id = "modal-editar">
-        <b-form-input v-model = "dados.status"></b-form-input>
-        <b-form-input v-model = "dados.categoria" class="mt-2"></b-form-input>
-        <b-form-input v-model = "dados.descricao" class="mt-2"></b-form-input>
-        <b-button @click="deletar" class="mt-5">Excluir</b-button>
-        <b-button @click="editarItem" class="mt-5">Editar</b-button>
-      </b-modal>
-
+          <b-button @click="deletar" class="mt-3 botaoModal" block>Excluir</b-button>
+          <b-button @click="editarItem" class="mt-3 botaoModal" block>Editar</b-button>
+        </b-modal>
       </b-row>
-    <b-row class="justify-content-end">
-      <b-pagination
-        v-model="currentPage"
-        :total-rows="rows"
-        :per-page="perPage"
-        first-number
-        last-number
-        class="pagination"
-        style="margin-right: 90px; margin-bottom: 22px; padding-right:15px;"
-      />
-    </b-row>
+      <b-row class="justify-content-end">
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="rows"
+          :per-page="perPage"
+          first-number
+          last-number
+          class="pagination"
+          style="margin-right: 90px; margin-bottom: 22px; padding-right:15px;"
+        />
+      </b-row>
     </b-container>
   </body>
 </template>
@@ -127,11 +129,11 @@
 
     editarItem(){
       putItem(this.dados.itemid,this.dados)
-      .then(()=>{
-        alert('editado')
-      }).catch((err)=>{
-        console.error(err)
-      });
+        .then(()=>{
+          alert('editado')
+        }).catch((err)=>{
+          console.error(err)
+        });
       this.$bvModal.hide("modal-editar");
     },
 
@@ -143,14 +145,14 @@
   },
  
   mounted(){
-            getItens()
-                .then((res)=>{
-                    this.items = res.data;
-                })
-                .catch((err)=>{
-                    console.log(err);
-                });
-        },
+    getItens()
+      .then((res)=>{
+          this.items = res.data;
+      })
+      .catch((err)=>{
+          console.log(err);
+      });
+  },
   computed: {
     rows() {
       return this.items.length
@@ -227,7 +229,6 @@ thead{
   justify-content: center;
 }
 
-
 .div-icones {
   transition: 300ms;
   .icons{
@@ -241,6 +242,12 @@ thead{
 
 .finalizar, .fotos, .excluir {
   margin-left: 5px;
+}
+
+.botaoModal{
+  background-color:#042d5b !important;
+  border: #042d5b !important;
+  font-family: Montserrat;
 }
 
 </style>
